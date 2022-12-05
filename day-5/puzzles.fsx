b4cc8fbd97161473145982e704693aaa19702bef
Samuel Dufour <dufoursamuel.fr@gmail.com>
2022-12-05 22:58:01 +0100

    Day 5 - Refactored

--- day-5/puzzles.fsx
+++ day-5/puzzles.fsx
@@ -2,32 +2,40 @@ open System.IO
 open System
 open System.Collections.Generic
 
-let input = File.ReadLines($"{__SOURCE_DIRECTORY__}/input.txt")
+let lines = File.ReadLines($"{__SOURCE_DIRECTORY__}/input-test.txt")
 
 type Action =
     { number: int
       origin: string
       dest: string }
 
-let stackLines = input |> Seq.takeWhile (fun l -> l <> "")
-let length = Seq.length stackLines
+let emptyLineIndex =
+    lines
+    |> Seq.indexed
+    |> Seq.filter (fun (_, l) -> l = String.Empty)
+    |> Seq.map fst
+    |> Seq.head
 
-let keys =
-    stackLines
-    |> Seq.last
-    |> (fun l -> l.Split(" ", StringSplitOptions.RemoveEmptyEntries))
+let stackLinesCount = emptyLineIndex
+let firstActionIndex = emptyLineIndex + 1
 
-let mapper l =
-    [ for idx in 1..4 .. String.length l do
-          l[idx] ]
+let extractStacks lines =
+    let stackLines = lines |> Seq.takeWhile ((<>) "")
 
-let stacksContent =
-    stackLines
-    |> Seq.take (length - 1)
-    |> Seq.map mapper
-    |> Array.ofSeq
+    let keys =
+        stackLines
+        |> Seq.last
+        |> (fun l -> l.Split(" ", StringSplitOptions.RemoveEmptyEntries))
 
-let rStacks =
+    let stacksContent =
+        let mapper l =
+            [ for idx in 1..4 .. String.length l do
+                  l[idx] ]
+
+        stackLines
+        |> Seq.take stackLinesCount
+        |> Seq.map mapper
+
     [ for idx in 0 .. keys.Length - 1 do
           keys[idx],
           stacksContent
@@ -37,59 +45,45 @@ let rStacks =
           |> Stack ]
     |> dict
 
-let actions =
-    input
-    |> Seq.skip (length + 1)
-    |> Seq.map (fun l -> l.Split([| "move"; "from"; "to"; " " |], StringSplitOptions.RemoveEmptyEntries))
-    |> Seq.map (fun parts ->
-        { number = int parts[0]
-          origin = parts[1]
-          dest = parts[2] })
+let extractActions lines =
+    let mapper (l: string) =
+        l.Split([| "move"; "from"; "to"; " " |], StringSplitOptions.RemoveEmptyEntries)
+        |> (fun parts ->
+            { number = int parts[0]
+              origin = parts[1]
+              dest = parts[2] })
+
+    lines
+    |> Seq.skip firstActionIndex
+    |> Seq.map mapper
     |> List.ofSeq
 
-// let doActions actions (rStacks: IDictionary<string, Stack<char>>) =
-//     let rec loop actions =
-//         match actions with
-//         | [] -> rStacks
-//         | { number = number
-//             origin = origin
-//             dest = dest } :: tail ->
-//             [ for _ in 1..number do
-//                   let extracted = rStacks[ origin ].Pop()
-//                   rStacks[ dest ].Push(extracted) |> ignore ]
-//             |> ignore
+let doActions lines revertOrder =
+    let stacks = extractStacks lines
 
-//             loop tail
-
-//     loop actions
-
-// let re = doActions actions rStacks
-
-// [| for k in re.Keys do
-//        re[ k ].Pop() |]
-// |> System.String
-
-let doActions actions (rStacks: IDictionary<string, Stack<char>>) =
     let rec loop actions =
         match actions with
-        | [] -> rStacks
+        | [] -> stacks
         | { number = number
             origin = origin
             dest = dest } :: tail ->
             let extracted =
                 [ for _ in 1..number do
-                      rStacks[ origin ].Pop() ]
+                      stacks[ origin ].Pop() ]
 
-            extracted
-            |> List.rev
-            |> List.iter (fun ex -> rStacks[ dest ].Push(ex))
+            if revertOrder then
+                List.rev extracted
+            else
+                extracted
+            |> List.iter (fun ex -> stacks[ dest ].Push(ex))
 
             loop tail
 
-    loop actions
+    let finalState = extractActions lines |> loop
 
-let re = doActions actions rStacks
+    [| for k in finalState.Keys do
+           finalState[ k ].Pop() |]
+    |> String
 
-[| for k in re.Keys do
-       re[ k ].Pop() |]
-|> System.String
+let puzzle1 = doActions lines false
+let puzzle2 = doActions lines true
